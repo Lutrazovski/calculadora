ca03162bb0de6c1813b44bf7508936377626a4a1
lutrazovski <lutrazovski@gmail.com>
2024-06-06 19:13:59 -0300

    Calculadora Inicio

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import calculadora.calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        calculadora calculadora = new calculadora();
+
+        calculadora.soma(2 ,3);
+    }
+}
---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+public class calculadora {
+    public void soma(int a, int b) {
+        int resultado = a + b;
+        System.out.println("Resultado da sua soma é: " + resultado);
+    }
+    public void subtrai(int a, int b) {
+        int resultado = a + b;
+        System.out.println("Resultado da sua subtracao é: " + resultado);
+    }
+    public void multiplica(int a, int b) {
+        int resultado = a * b;
+        System.out.println("Resultado da sua multiplicacao é: " + resultado);
+    }
+    public void divide(int a, int b) {
+        float resultado = a / b;
+        System.out.println("Resultado da sua divisao é: " + resultado);
+    }
+}
